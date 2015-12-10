7fe6d4cca1bbfe41b1940be35214e07002720fd3
Brian Connelly <briandconnelly@users.noreply.github.com>
2015-12-09 17:34:54 -0800

    Figure S1E Updates (renamed files)

--- scripts/plot-migrationsweep.R
+++ scripts/plot-migrationsweep.R
@@ -5,10 +5,10 @@ source('hankshaw.R')
 # How often data were logged
 data_interval <- 1
 
-data_fig2e <- read.csv('../data/migrationsweep.csv.bz2')
-data_fig2e$Replicate <- as.factor(data_fig2e$Replicate)
+d <- read.csv('../data/migrationsweep.csv.bz2')
+d$Replicate <- as.factor(d$Replicate)
 
-data_fig2e_integral <- data_fig2e %>%
+presence <- d %>%
     group_by(MigrationRate, Replicate) %>%
     summarise(Integral=data_interval * sum(CooperatorProportion)/(max(Time)-min(Time)))
 
@@ -20,15 +20,15 @@ migration_labels_log <- c(expression(paste(5, 'x', 10^{-7})),
                           expression(bold(paste('5', 'x', '10'^{'-2'}))),
                           expression(paste(5, 'x', 10^{-1})))
 
-fig2e <- ggplot(data_fig2e_integral, aes(x=MigrationRate, y=Integral)) +
+fig2e <- ggplot(data=presence, aes(x=MigrationRate, y=Integral)) +
     draw_replicates() +
     stat_summary(fun.data='figsummary', size=point_size) +
-    scale_x_log10(breaks=unique(data_fig2e_integral$MigrationRate),
+    scale_x_log10(breaks=unique(presence$MigrationRate),
                   labels=migration_labels_log) +
     scale_y_continuous(limits=c(0, 1)) +
     labs(x=figlabels['migration_rate'], y=figlabels['producer_presence']) +
     theme_hankshaw(base_size=textbase_2wide)
 fig2e <- rescale_golden(plot=fig2e)
 
-save_figure(filename='../figures/Figure2e.png', plot=fig2e, label='E',
-            trim=TRUE)
+save_figure(filename='../figures/migrationsweep-integral.png', plot=fig2e,
+            label='E', trim=TRUE)
